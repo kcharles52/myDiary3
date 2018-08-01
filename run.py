d6b5b6cd6455f527d8d9574c6bdcdb4d39e220d1
charles K <kcharles52@gmail.com>
2018-08-01 11:45:48 +0300

    Attach configuration to instance app

--- run.py
+++ run.py
@@ -1,14 +1,13 @@
-from flask import Flask
-from config import app_configuration
+from app import instance
+from app.config import app_configuration
 
 
 def create_app(mode):
     """This method is used to create a flask application with the specified mode"""
-    app = Flask(__name__)
+    app = instance.app
 
     app.config.from_pyfile('config.py')
     app.config.from_object(app_configuration[mode])
-
     return app
 
 
