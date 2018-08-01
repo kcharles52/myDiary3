8240e2549a8d75f2dea58e749b303cac05ccba68
charles K <kcharles52@gmail.com>
2018-08-01 08:12:08 +0300

    create entries route

--- app/views/entries_view.py
+++ app/views/entries_view.py
@@ -3,7 +3,34 @@ import datetime
 
 from ..models.diaryEntries_model import DiaryEntry
 
+
 #create entries blueprint
 entries = Blueprint('entries',__name__)
 
 
+@entries.route("/api/v1/entries", methods=["POST"])
+def create_entry():
+    """ Endpoint to create a diary entry given the entry data """
+    # get request data
+    diary_entry_data = request.get_json()
+
+    #check if entry data is not empty
+    if not diary_entry_data:
+        return jsonify({"message": "Enter data in all fields"}), 400
+
+    diaryTitle = diary_entry_data.get('diaryTitle')
+    date = str(diary_entry_data.get('date')).strip()
+    diaryEntryBody = diary_entry_data.get('diaryEntryBody')
+
+    # validate request data
+    if not diaryTitle or diaryTitle == "" or diaryTitle == type(int):
+        return jsonify({'Message': 'Title is required'}), 400
+    if not date or date == "":
+        return jsonify({'Message': 'date is required'}), 400
+    if not diaryEntryBody or diaryEntryBody == "":
+        return jsonify({'Message': 'Field required: Please write someting'}), 400
+
+    new_diary_entry = DiaryEntry(diaryTitle, date, diaryEntryBody, user_id)
+
+
+    return jsonify({'Message': 'You have successfully created your entry'}), 201
