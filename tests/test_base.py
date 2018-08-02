4ba7258f0a1ed67dbe9950a0ade3019710f6eceb
charles K <kcharles52@gmail.com>
2018-08-02 23:53:41 +0300

    Edit base test class

--- tests/test_base.py
+++ tests/test_base.py
@@ -6,9 +6,8 @@ from db import  DatabaseConnection
 connection = DatabaseConnection()
 
 
-#Base test class for users
-class BaseTestCaseUser(unittest.TestCase):
-    """"This is the base class for user test class"""
+class BaseTestCase(unittest.TestCase):
+    """"This is the base class for test class"""
     def setUp(self):
         self.test_client = app.test_client()
         self.user_register_data = {
@@ -20,22 +19,6 @@ class BaseTestCaseUser(unittest.TestCase):
             "email": "kato@gmail.com",
             "password": "123456"
         }
-
-    def tearDown(self):
-        cursor = connection.cursor
-        cursor.execute("""TRUNCATE TABLE users RESTART IDENTITY CASCADE""")
-
-
-
-
-class BaseTestCaseDiaryEntry(unittest.TestCase):
-    def setUp(self):
-        self.test_client = app.test_client()
-        self.user_register_data = {
-            "name": "Kato",
-            "email": "kato@gmail.com",
-            "password": "123456"
-        }
         self.diary_entry_data = {
             "diaryTitle": "wedding Dm",
             "date": "1/2/2017",
@@ -43,6 +26,15 @@ class BaseTestCaseDiaryEntry(unittest.TestCase):
             "user_id":"1"
         }
 
+        self.modified_diary_entry_data = {
+            "diaryTitle": "Modified",
+            "date": "1/2/2018",
+            "diaryEntryBody": "This entry has been modified"
+        }
     def tearDown(self):
         cursor = connection.cursor
-        cursor.execute("""TRUNCATE TABLE entries , users RESTART IDENTITY CASCADE""")
+        # cursor.execute("""TRUNCATE TABLE users RESTART IDENTITY CASCADE""")
+        cursor.execute(
+            """TRUNCATE TABLE entries , users RESTART IDENTITY CASCADE""")
+
+
--- tests/test_diaryEntries.py
+++ tests/test_diaryEntries.py
@@ -1,10 +1,10 @@
 import  json
 from app.instance import app
-from .test_base import BaseTestCaseDiaryEntry
+from .test_base import BaseTestCase
 
 
 
-class EntriesTest(BaseTestCaseDiaryEntry):
+class EntriesTest(BaseTestCase):
         #tests for creating diary entry
     def test_create_entry(self):
         """ Tests whether a user can create an entry successfully """
@@ -89,3 +89,25 @@ class EntriesTest(BaseTestCaseDiaryEntry):
             '/api/v1/entries/3', content_type='application/json')
         self.assertEqual(response.status_code, 404)
         self.assertIn('Diary Entry Not Found', str(response.data))
+
+        #tests for modifying entry
+
+    def test_modify_empty_entry(self):
+        """ Tests whether a user can modify when there are no entries """
+        response = self.test_client.put(
+            '/api/v1/entries/1', data=json.dumps(self.diary_entry_data), content_type='application/json')
+        self.assertEqual(response.status_code, 404)
+        self.assertIn("You have no entries to modify", str(response.data))
+
+    def test_modify_entry(self):
+        """ Tests whether a user can modify an entry successfully """
+        self.test_client.post(
+            '/api/v1/auth/signup', data=json.dumps(self.user_register_data),
+            content_type='application/json')
+        self.test_client.post(
+            '/api/v1/entries', data=json.dumps(self.diary_entry_data), content_type='application/json')
+        response = self.test_client.put(
+            '/api/v1/entries/1', data=json.dumps(self.modified_diary_entry_data), content_type='application/json')
+        self.assertEqual(response.status_code, 201)
+        self.assertIn("You successfully modified your entry",
+                      str(response.data))
