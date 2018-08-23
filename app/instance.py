66b1452bb301c90dc932b467e88dceb8a0f78786
charles K <kcharles52@gmail.com>
2018-08-23 13:43:46 +0300

    Edit CORS resource

--- app/instance.py
+++ app/instance.py
@@ -1,9 +1,10 @@
 #imports
 from flask import Flask
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
 from .views.users_auth import users
 from .views.entries_view import entries
 import db
+import logging
 
 db_instance = db.DatabaseConnection()
 db_instance.create_table_users()
@@ -13,8 +14,8 @@ db_instance.create_table_entries()
 
 #create flask application
 app = Flask(__name__)
-CORS(app)
-
+CORS(app, resources=r'/api/*')
+# logging.getLogger('flask_cors').level = logging.DEBUG
 #Add blueprints to the flask application instance
 app.register_blueprint(users, url_prefix='/api/v1/auth')
 app.register_blueprint(entries, url_prefix='/api/v1')
