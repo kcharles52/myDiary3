2d704a00c8bff1468c61cd06dd7517017f9b0d69
charles K <kcharles52@gmail.com>
2018-08-26 09:33:11 +0300

    User login function

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -20,9 +20,11 @@ function switchTab(evt, tabName) {
 
 }
 
+
+
 //get active tab content
-var activeTab = document.getElementsByClassName("activeTab")
-activeTab[0].style.display="block"
+// var activeTab = document.getElementsByClassName("activeTab")
+// activeTab[0].style.display="block"
 
 function openTab(evt, tabName) {
     // Declare all variables
@@ -50,4 +52,64 @@ function openTab(evt, tabName) {
     currentTab.className = className.replace("tabcontent","activeTab");
     evt.currentTarget.className += " active";
 
+}
+
+// let host_server = "http://127.0.0.1:5000"
+//function to register user
+function register_user() {
+    let url = "http://127.0.0.1:5000/api/v1/auth/signup";
+    let Fullname = document.getElementById('FName').value + " " + document.getElementById('LName').value
+    fetch(url, {
+            method: 'POST',
+            mode: 'cors',
+            redirect:'manual',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "name": Fullname,
+                "email": document.getElementById("email").value,
+                "password": document.getElementById("password").value
+            })
+        })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function(data){
+           if(true){
+               alert(data['message'])
+               window.location.href="index.html"
+           }
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
+function login() {
+    fetch('http://127.0.0.1:5000/api/v1/auth/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            'email': document.getElementById('logEmail').value,
+            'password': document.getElementById('password').value
+        }),
+        mode: 'cors',
+        redirect: 'manual'
+    })
+        .then(Response => Response.json())
+        .then(reply => {
+            if (!reply['Message']) {
+                let key = 'token';
+                let value = reply['token'];
+                localStorage.setItem(key, value);
+                console.log(localStorage.getItem('token'));
+                window.location.href = 'welcome.html';
+            } else if (reply['Message']) {
+                return alert(reply['Message'])
+            }
+            return true;
+        })
+        .catch(error => alert(error))
 }
