24a7173f5f5082bda79fb6c75c2b61e6b79924e0
charles K <kcharles52@gmail.com>
2018-08-26 09:32:11 +0300

    create user interface routes

--- app/views/UI.py
+++ app/views/UI.py
@@ -4,6 +4,15 @@ from flask import Blueprint, render_template
 #create blue print
 UI = Blueprint('UI',__name__)
 
-@UI.route('/' )
+@UI.route('/', methods=['GET','POST'])
+@UI.route('/index.html', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
+
+@UI.route('/signup.html', methods=['GET', 'POST'])
+def signup():
+    return render_template('signup.html')
+
+@UI.route('/welcome.html', methods=['GET'])
+def home():
+    return render_template('welcome.html')
