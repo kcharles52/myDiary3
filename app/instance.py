bb955b14c82383195afadcf43950312bf9a5eafb
Kato Charles <katocharles@Katos-MacBook-Pro.local>
2018-09-06 14:55:11 +0300

    update requirements

--- app/instance.py
+++ app/instance.py
@@ -1,5 +1,6 @@
 #imports
 from flask import Flask
+from flask_cors import CORS
 from .views.users_auth import users
 from .views.entries_view import entries
 import db
@@ -12,7 +13,7 @@ db_instance.create_table_entries()
 
 #create flask application
 app = Flask(__name__)
-
+CORS(app)
 #Add blueprints to the flask application instance
 app.register_blueprint(users, url_prefix='/api/v1/auth')
 app.register_blueprint(entries, url_prefix='/api/v1')
